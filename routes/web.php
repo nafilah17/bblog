15be67f320fb48f596223d9ec622a7f8ccdb70de
nafilah17 <rissanafilah@gmail.com>
2020-07-14 19:27:42 +0700

    insert template on blog post and admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog', 'BlogController@index');
+
+
+Route::get('/admin', 'AdminController@index');
+
+Route::get('/article', 'ArticleController@index');
+
+Route::get('/category', 'CategoriesController@index');
+
+Route::get('/profile', 'ProfileController@index');
+
+Route::get('/user', 'UserController@index');
